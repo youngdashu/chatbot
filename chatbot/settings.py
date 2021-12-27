5bc00b4cb00deb4f5893c400e5257337629daba2
youngdashu <anowak2000@gmail.com>
2021-12-27 14:16:46 +0100

    db from env commented

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -93,8 +93,8 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['production'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['production'].update(db_from_env)
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
