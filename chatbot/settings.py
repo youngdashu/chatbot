c8849110e5bbdf419947d70edc8eda8a6b085125
youngdashu <anowak2000@gmail.com>
2021-12-27 13:17:16 +0100

    gitignore env

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m))6-qbez&d-%f()!op=d6-d-*$qb-rlw06iyw97ob11flp$^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'tire-company-chatbot-app.herokuapp.com']
 
 
 # Application definition
