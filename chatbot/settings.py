1a2d521294dc3c80906c350ade2d4f687cb14a7b
youngdashu <anowak2000@gmail.com>
2021-12-27 13:59:48 +0100

    django_heroku

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import dj_database_url
 import gunicorn
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
